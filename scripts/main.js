a79d93c4d9eb22f8b2bc909cff963bfe596731b0
Mayank-Khurmaim <mayankkhurmai8@gmail.com>
2022-02-26 21:53:26 +0530

    Final Commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,44 @@
-function openTab(event, divID) 
-{
-    let tabcontent = document.getElementsByClassName("tabcontent");
-    let tablinks = document.getElementsByClassName("tablinks");
+document.getElementById("add-row").addEventListener('click', ()=>{
+  let trCount = document.getElementsByClassName("tr-count").length+1;
     
-    for(let i = 0; i < tabcontent.length; i++)
-    {
-      tabcontent[i].style.display = "none";
-    }
+  let tableRef = document.querySelector('TABLE');
+  let newRow = tableRef.insertRow(-1);
+  newRow.className += "tr-count";
+
+  let td1 = newRow.insertCell(0);
+  td1.innerHTML = "<b>"+trCount+"</b>";
+
+  let td2 = newRow.insertCell(1);
+  td2.innerHTML = "User Name "+ trCount;
+
+  let td3 = newRow.insertCell(2);
+  td3.innerHTML = Math.floor((Math.random() * 100000));
   
-    for(let i = 0; i < tablinks.length; i++)
-    {
-      tablinks[i].className = tablinks[i].className.replace("active", "");
-    }
+  let td4 = newRow.insertCell(3);
+  td4.innerHTML = "<div class='icons8-delete' id='delete' onclick='delete_row(this)'></div>";
 
-    document.getElementById(divID).style.display = "block";
-    event.currentTarget.className += " active";
-  }
+});
 
-  document.getElementById("defaultOpen").click();
+
+delete_row = (trNum)=>{
+  var x = trNum.parentNode.parentNode.rowIndex;
+  document.querySelector('TABLE').deleteRow(x);
+}
+
+
+document.getElementById("change-css").addEventListener('click', ()=>{
+  let bgcolors = ["red", "pink", "orange","aliceblue", "green", "yellow"];
+
+  let index = document.querySelector('#change-css').getAttribute("value");
+  
+  index++;
+  if(index>bgcolors.length)
+      index = 1;
+  document.querySelector('BODY').style.backgroundColor = bgcolors[index-1];
+  document.querySelector('#change-css').setAttribute("value", index);
+});
+
+
+document.getElementById("reset").addEventListener('click', ()=>{
+  location.reload();
+});
